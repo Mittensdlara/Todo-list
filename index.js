fa00628f553c8b1eee61e45b5f8c6840b9f8868d
Mittensdlara <pooraghaeedelara@gmail.com>
2022-11-17 19:40:53 +0330

    Implement tabs switch functionality failed

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 document.querySelector('#task-submit').onclick = function(){
-    if(document.querySelector('#new-task input').value.length == 0){
+    if(document.querySelector('#new-task #task-input').value.length == 0){
         alert("Please Enter a Task")
     }
     else{
         document.querySelector('#tasks').innerHTML += `
             <div class="task">
                 <span id="task-name">
-                    ${document.querySelector('#new-task input').value}
+                    ${document.querySelector('#new-task #task-input').value}
                 </span>
                 <button class="delete"> Delete
-                    <i class="far fa-trash-alt"></i>
+                    
                 </button>
             </div>
         `;
+         
+
 
         var current_tasks = document.querySelectorAll(".delete");
         for(var i=0; i<current_tasks.length; i++){
             current_tasks[i].onclick = function(){
                 this.parentNode.remove();
             }
-        
+
         }
 
         document.querySelector("#new-task input").value = "";
     }
 }
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = "imporatnt";
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
