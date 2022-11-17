21d7fdb99db3655c069241f685b1b1e78811eda8
Mittensdlara <pooraghaeedelara@gmail.com>
2022-11-17 15:04:23 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+document.querySelector('#task-submit').onclick = function(){
+    if(document.querySelector('#new-task input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="task-name">
+                    ${document.querySelector('#new-task input').value}
+                </span>
+                <button class="delete"> Delete
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+               
+            }
+        }
+
+        document.querySelector("#new-task input").value = "";
+    }
+}
